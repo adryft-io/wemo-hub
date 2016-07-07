2e0f9047bc6d0bbf00115ca36ea3ff11a1dc9fb4
Nam Dao <nam.d.dao@gmail.com>
2016-07-06 20:06:07 -0700

    wemo hub to control belkin smart switches

--- hub.js
+++ hub.js
@@ -24,33 +24,38 @@ wemo.load('http://10.6.24.194:49153/setup.xml', function (deviceInfo) {
   console.log(deviceInfo.UDN.slice(5));
   var message;
   socket.on('message', function (msg) {
-    console.log('Received: ', msg);
-    message = msg.match(/turn on|turn off/i)[0];
-    console.log('message after reg ex', message);
-    wemoClient.getBinaryState((err,data) => {
-      console.log('data is', data);
-      if (err) return console.log(err);
-      if (data === '8' && message === 'turn off') {
-        wemoClient.setBinaryState(0, (err, res) => {
-          if (err) {
-            console.log('err is', err);
-          } else {
-            console.log('res from turn off code is', res);
-          }
-        });
-      }
-      if (data === '0' && message === 'turn on') {
-        wemoClient.setBinaryState(1, (err, res) => {
-          if (err) {
-            console.log('err is', err);
-          } else {
-            console.log('res from turn off code is', res);
-          }
-        });
-      }
-      wemoClient.on('binaryState', (value) => {
-        console.log('current value is', value);
-      });   
-    });      
-  });
+    console.log('Received: ', typeof JSON.parse(msg));
+    msg = JSON.parse(msg);
+    console.log('wemo id', msg.id);
+    console.log('message body', msg.action_props.body);
+    if (msg.reaction_fields.wemoId) { 
+      message = msg.action_props.body.match(/turn on|turn off/i)[0].toLowerCase();
+      console.log('message after reg ex', message);
+      wemoClient.getBinaryState((err,data) => {
+        console.log('data is', data);
+        if (err) return console.log(err);
+        if (data === '8' && message === 'turn off') {
+          wemoClient.setBinaryState(0, (err, res) => {
+            if (err) {
+              console.log('err is', err);
+            } else {
+              console.log('res from turn off code is', res);
+            }
+          });
+        }
+        if (data === '0' && message === 'turn on') {
+          wemoClient.setBinaryState(1, (err, res) => {
+            if (err) {
+              console.log('err is', err);
+            } else {
+              console.log('res from turn off code is', res);
+            }
+          });
+        }
+        wemoClient.on('binaryState', (value) => {
+          console.log('current value is', value);
+        });   
+      }); 
+    }     
+  })
 });   
