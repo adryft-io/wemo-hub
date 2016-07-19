bfc737b956297f71f551dcdf75939ede1f96251d
Nam Dao <nam.d.dao@gmail.com>
2016-07-18 18:01:21 -0700

    code cleanup to delete unnecessary console.logs

--- hub.js
+++ hub.js
@@ -5,36 +5,29 @@ var request      = require( 'request' );
 var http = require('http');
 var server = http.createServer(function(request, response){
   response.writeHead(200, {'Content-Type': 'text/html'});
-  response.write('hello world');
   response.end();
 });
 var client = require("socket.io-client"); 
 var socket = client.connect("http://localhost:8080", {'multiplex': false});
 
 socket.on('connect', function (socket) {
-  console.log('hello world');
+  console.log('connected to server');
 });
 
 server.listen(8060); 
 
 wemo.load('http://10.6.24.194:49153/setup.xml', function (deviceInfo) {
-  console.log(deviceInfo);
-  console.log('yoyo');
   var wemoClient = wemo.client(deviceInfo);
   console.log(deviceInfo.UDN.slice(5));
   var message;
   socket.on('message', function (msg) {
-    console.log('Received: ', typeof JSON.parse(msg));
     msg = JSON.parse(msg);
-    console.log('wemo id', msg.id);
-    console.log('message body', msg.action_props.body);
     if (msg.reaction_fields.wemoId) { 
       message = msg.action_props.body.match(/turn on|turn off/i)[0].toLowerCase();
       console.log('message after reg ex', message);
       wemoClient.getBinaryState((err,data) => {
-        console.log('data is', data);
         if (err) return console.log(err);
-        if (data === '8' && message === 'turn off') {
+        if (data !== '0' && message === 'turn off') {
           wemoClient.setBinaryState(0, (err, res) => {
             if (err) {
               console.log('err is', err);
